1a825be4b853da7a4d9cb7dc2ef9e690f53cbd62
YaroslavOnuchak <onuchak.j@gmail.com>
2020-12-08 09:25:42 +0200

    add burger

--- app/js/main.js
+++ app/js/main.js
@@ -3,15 +3,27 @@
 @@include("_script.js");
 
 $(document).ready(function () {
+  let countSlidesToShow = 5;
+  function resizeW() {
+    if (window.innerWidth < 700 && window.innerWidth > 400) {
+      countSlidesToShow = 4;
+    } else if (window.innerWidth < 400) {
+      countSlidesToShow = 3;
+    } else {
+      countSlidesToShow = 5;
+    }
+  }
+  resizeW();
+
   $(".slider").slick({
-    slidesToShow: 5,
+    slidesToShow: countSlidesToShow,
     arrows: true,
     dots: true,
     adaptiveHeight: true,
     slidesToScroll: 2,
     infinite: true,
     // accessibility: false,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 5000,
     // centerPadding: "50px",
     // speed: 5000,
@@ -22,4 +34,6 @@ $(document).ready(function () {
     pauseOnHover: false,
     easing: "cubic-bezier(.37,-0.58,.84,1.04)",
   });
+
+  $(window).resize(resizeW);
 });
--- uneke/js/main.js
+++ uneke/js/main.js
@@ -44,15 +44,27 @@ testWebP(function (support) {
 ;
 
 $(document).ready(function () {
+  let countSlidesToShow = 5;
+  function resizeW() {
+    if (window.innerWidth < 700 && window.innerWidth > 400) {
+      countSlidesToShow = 4;
+    } else if (window.innerWidth < 400) {
+      countSlidesToShow = 3;
+    } else {
+      countSlidesToShow = 5;
+    }
+  }
+  resizeW();
+
   $(".slider").slick({
-    slidesToShow: 5,
+    slidesToShow: countSlidesToShow,
     arrows: true,
     dots: true,
     adaptiveHeight: true,
     slidesToScroll: 2,
     infinite: true,
     // accessibility: false,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 5000,
     // centerPadding: "50px",
     // speed: 5000,
@@ -63,4 +75,6 @@ $(document).ready(function () {
     pauseOnHover: false,
     easing: "cubic-bezier(.37,-0.58,.84,1.04)",
   });
+
+  $(window).resize(resizeW);
 });
